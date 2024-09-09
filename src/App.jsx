76392a52a3bf5dd8a88b2db846a4acd7a029d1b3
felipenunes <felipenuneto@gmail.com>
2024-09-09 13:48:09 -0300

    Commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import { useState,useEffect } from 'react'
+import './App.css'
+
+const url = "http://localhost:3000/produtos"
+
+function App() {
+  const [produtos, setProdutos] = useState([])
+  const [nome, setNome] = useState("")
+  const [preco, setPreco] = useState(0)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const prod = {
+      nome,
+      preco
+    }
+
+    const res = await fetch(url)
+    const add = await res.json()
+  }
+
+  useEffect(() => {
+    async function fetchData(){
+      const link = await fetch(url)
+      const prods = await link.json()
+      setProdutos(prods)
+      console.log(prods)
+    }
+    fetchData()
+  },[])
+  return (
+    <div className='App'>
+      <h1>LISTA DE PRODUTOS</h1>
+      <ul>
+        {produtos.map((prod)=>(
+          <li key={prod.id}> {prod.nome} - {prod.preco}</li>
+        )
+        )}
+      </ul>
+
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            <span>Digite o nome do produto</span>
+            <input 
+              type="text" 
+              placeholder="Nome produto" 
+              value={nome}
+              onChange={(e) => setNome(e.target.value)}/>
+          </label>
+          <label>
+            <span>Digite o preço do produto</span>
+            <input 
+              type="number" 
+              placeholder='Preço do produto' 
+              min={0} 
+              value={preco}
+              onChange={(e)=>setPreco(e.target.value)}/>
+          </label>
+          <input type="submit"  value="Enviar"/>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default App
